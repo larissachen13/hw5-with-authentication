c267d6d8dc8f081f53c2039dfdb42e5258eeb010
Larissa Chen <larissachen13@gmail.com>
2016-08-08 15:44:30 -0400

    updated server.js

---
+++ app/controllers/post_controller.js
@@ -0,0 +1,69 @@
+import Post from '../models/post_model';
+
+export const createPost = (req, res) => {
+  const post = new Post();
+  post.title = req.body.title;
+  post.tags = req.body.tags;
+  post.content = req.body.content;
+  post.save()
+    .then(result => {
+      res.json({ message: 'Post created!' });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+};
+
+export const getPosts = (req, res) => {
+  Post.find()
+  .then(posts => {
+    // this cleans the posts because we use id instead of dangling _id
+    // and we purposefully don't return content here either
+    const cleanPosts = posts.map(post => {
+      return { id: post._id, title: post.title, tags: post.tags, content: post.content };
+    });
+    res.json(cleanPosts);
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+export const getPost = (req, res) => {
+  const id = req.params.id;
+  Post.findById(id)
+  .then(post => {
+    post = { id: post._id, title: post.title, tags: post.tags, content: post.content };
+    res.json(post);
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+export const deletePost = (req, res) => {
+  const id = req.params.id;
+  Post.findByIdAndRemove(id)
+  .then(result => {
+    res.json(result);
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+
+export const updatePost = (req, res) => {
+  const id = req.params.id;
+  const post = new Post();
+  post.title = req.body.title;
+  post.tags = req.body.tags;
+  post.content = req.body.content;
+  Post.findByIdAndUpdate(id, post)
+  .then(result => {
+    res.json(result);
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import apiRouter from './router';
 
 // initialize
 const app = express();
@@ -12,15 +14,25 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', apiRouter);
 
-// default index route
-app.get('/', (req, res) => {
-  res.send('hi');
-});
 
 // START THE SERVER
 // =============================================================================
 const port = process.env.PORT || 9090;
 app.listen(port);
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
+// default index route
+app.get('/', (req, res) => {
+  res.send('hi');
+});
 
 console.log(`listening on: ${port}`);
